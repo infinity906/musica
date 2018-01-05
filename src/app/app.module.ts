3185b629942d79088fa2aae9f51b6609f71c5616
Pratik Shetty <pratik906@gmail.com>
2018-01-05 11:33:55 -0500

    created new app, added images, used loading controller to show loading message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { MusicProvider } from '../providers/music/music';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
@@ -28,7 +31,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    MusicProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, LoadingController } from 'ionic-angular';
+import { MusicProvider } from "../../providers/music/music"
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public allMusic = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(private loadingController: LoadingController, private musicProvider: MusicProvider, public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad(){
+    let allMusicloadingController = this.loadingController.create({
+      content: "Getting you content from server"
+    });
+    allMusicloadingController.present();
+    this.musicProvider.getMusic()
+    .subscribe((musiclist) => {
+      allMusicloadingController.dismiss();
+      this.allMusic = musiclist
+    })
   }
 
 }
